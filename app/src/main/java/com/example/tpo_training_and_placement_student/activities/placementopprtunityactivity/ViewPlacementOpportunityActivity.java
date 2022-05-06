87b35ba54cd7e28915c2e93f15da1463113c8a3d
Nishigandha Ingle <nishigandhaingle2403@gmail.com>
2022-05-06 11:45:09 +0530

    activity_view_placement_opportunity.xml and ViewPlacementOpportunityActivity.java are added

---
+++ app/src/main/java/com/example/tpo_training_and_placement_student/activities/placementopprtunityactivity/ViewPlacementOpportunityActivity.java
@@ -0,0 +1,16 @@
+package com.example.tpo_training_and_placement_student.activities.placementopprtunityactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tpo_training_and_placement_student.R;
+
+public class ViewPlacementOpportunityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_placement_opportunity);
+    }
+}
