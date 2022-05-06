cceece982b891b0b109a4e185e9dad41d8d8fe8d
Nishigandha Ingle <nishigandhaingle2403@gmail.com>
2022-05-06 11:44:04 +0530

    activity_edit_student_profile.xml and EditStudentProfileActivity.java are added

---
+++ app/src/main/java/com/example/tpo_training_and_placement_student/activities/profileactivity/EditStudentProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.tpo_training_and_placement_student.activities.profileactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tpo_training_and_placement_student.R;
+
+public class EditStudentProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_student_profile);
+    }
+}
