a58f4f0c49d3e1116cf4712143691c594ef33d10
Nishigandha Ingle <nishigandhaingle2403@gmail.com>
2022-05-06 11:46:43 +0530

    activity_view_student_profile.xml and ViewStudentProfileActivity.java are added

---
+++ app/src/main/java/com/example/tpo_training_and_placement_student/activities/profileactivity/ViewStudentProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.tpo_training_and_placement_student.activities.profileactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tpo_training_and_placement_student.R;
+
+public class ViewStudentProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_student_profile);
+    }
+}
