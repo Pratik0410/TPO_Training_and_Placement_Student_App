5ef3c0acb99f6c28ada66f45daeb6ac4c9c4cbd6
Nishigandha Ingle <nishigandhaingle2403@gmail.com>
2022-05-06 11:47:45 +0530

    activity_view_training.xml and ViewTrainingActivity.java are added

---
+++ app/src/main/java/com/example/tpo_training_and_placement_student/activities/training/ViewTrainingActivity.java
@@ -0,0 +1,17 @@
+package com.example.tpo_training_and_placement_student.activities.training;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tpo_training_and_placement_student.R;
+
+public class ViewTrainingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_training);
+
+    }
+}
