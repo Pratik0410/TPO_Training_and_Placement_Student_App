655ee8d750b0af8258b4ad7b4347755939193067
Nishigandha Ingle <nishigandhaingle2403@gmail.com>
2022-05-07 15:06:28 +0530

    ui_view_placement_opportunity.xml and ViewPlacementOpportunityUi.java files added

---
+++ app/src/main/java/com/example/tpo_training_and_placement_student/ui/ViewPlacementOpportunityUi.java
@@ -0,0 +1,16 @@
+package com.example.tpo_training_and_placement_student.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tpo_training_and_placement_student.R;
+
+public class ViewPlacementOpportunityUi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ui_view_placement_opportunity);
+    }
+}
