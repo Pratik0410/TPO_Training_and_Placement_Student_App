d088cd51bcfc603b95cc0814223b096b0bdf9aa6
Nishigandha Ingle <nishigandhaingle2403@gmail.com>
2022-05-08 15:15:01 +0530

    activity_view_placement_history.xml, model_placement_history_year_card.xml, model_view_placement_history_card.xml, model_pre_placement_card.xml, ViewPlacementHistoryActivity.java Added

---
+++ app/src/main/java/com/example/tpo_training_and_placement_student/activities/placementhistory/ViewPlacementHistoryActivity.java
@@ -0,0 +1,16 @@
+package com.example.tpo_training_and_placement_student.activities.placementhistory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tpo_training_and_placement_student.R;
+
+public class ViewPlacementHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_placement_history);
+    }
+}
